323715421e03326b0ca847d85d1cdcdf49b3d3b2
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-06-19 19:05:47 -0700

    prints all letters except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter = 97;
+
+	while (letter <= 122)
+	{
+		if (letter == 101 || letter == 113)
+		{
+			letter++;
+			continue;
+		}
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
