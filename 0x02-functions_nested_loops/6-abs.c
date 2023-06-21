e13a165556f83e026bd4908fa4790d66d4b52cf7
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-06-21 15:08:07 -0700

    computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,11 +7,12 @@
 
 int _abs(int a)
 {
-	if (a < 0)
-	{
-		int abs_value
-			abs_value = a * -1;
-		return (abs_value);
-	}
-	return (a);
+if (a < 0)
+{
+int abs_value;
+
+abs_value = a * -1;
+return (abs_value);
+}
+return (a);
 }
