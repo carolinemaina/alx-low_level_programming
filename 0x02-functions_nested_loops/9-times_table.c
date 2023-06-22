968497cac2aa57d281a567ca60a7585b8f53dc6f
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-06-22 00:00:31 -0700

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,21 +2,20 @@
 /**
  * times_table -  prints the 9 times table, starting with 0
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 
 void times_table(void)
 
 {
-int num1, num2, num3, num4, num5;
-
-for (num1 = 0; num1 <= 9; num1++)
-
-{
-	for (num2 = 0; num2 <= 9; num2++)
+	int num1, num2, num3, num4, num5;
+	
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		num3 = num1 * num2;
-		if (num3 > 9)
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			num3 = num1 * num2;
+			if (num3 > 9)
 			{
 				num4 = num3 % 10;
 				num5 = (num3 - num4) / 10;
@@ -25,7 +24,7 @@ for (num1 = 0; num1 <= 9; num1++)
 				_putchar(num5 + '0');
 				_putchar(num4 + '0');
 			}
-		else
+			else
 			{
 				if (num2 != 0)
 				{
@@ -36,6 +35,6 @@ for (num1 = 0; num1 <= 9; num1++)
 				_putchar(num3 + '0');
 			}
 		}
-	_putchar('\n');
-}
+		_putchar('\n');
+	}
 }
