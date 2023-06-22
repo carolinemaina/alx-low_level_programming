cbb0da4b6f42d7e2748e780f5c71fa797d9ff490
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-06-22 08:17:26 -0700

    finds and prints the first 98 Fibonacci numbers, starting with 1 and 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,40 +9,44 @@
 
 int main(void)
 {
-	int total;
-	unsigned long num1 = 0, num2 = 1, fibsum;
-	unsigned long num1a, num2a, num1b, num2b;
-	unsigned long a, b;
+        int total;
+        unsigned long num1 = 0, num2 = 1, fibsum;
+        unsigned long num1a, num1b, num2a, num2b;
+        unsigned long a, b;
 
-	for (total = 0; total < 92; total++)
-	{
-		fibsum = num1 + num2;
-		printf("%lu, ", fibsum);
-		num1 = num2;
-		num2 = fibsum;
-	}
-	num1a = num1 / 10000000000;
-	num2a = num2 / 10000000000;
-	num1b = num1 % 10000000000;
-	num2b = num2 % 10000000000;
+        for (total = 0; total < 92; total++)
+        {
+                fibsum = num1 + num2;
+                printf("%lu, ", fibsum);
 
-	for (total = 93; total < 99; total++)
-	{
-		a = num1a + num2a;
-		b = num1b + num2b;
-		if (num2b + num2b > 9999999999)
-		{
-			a += 1;
-			b %= 10000000000;
-		}
-		printf("%lu%lu", a, b);
-		if (total != 98)
-			printf(", ");
-		num1a = num2a;
-		num1b = num2b;
-		num2a = a;
-		num2b = b;
-	}
-	printf("\n");
-	return (0);
+                num1 = num2;
+                num2 = fibsum;
+        }
+
+        num1a = num1 / 10000000000;
+        num2a = num2 / 10000000000;
+        num1b = num1 % 10000000000;
+        num2b = num2 % 10000000000;
+
+        for (total = 93; total < 99; total++)
+        {
+                a = num1a + num2a;
+                b = num1b + num2b;
+                if (num1b + num2b > 9999999999)
+                {
+                        a += 1;
+                        b %= 10000000000;
+                }
+
+                printf("%lu%lu", a, b);
+                if (total != 98)
+                        printf(", ");
+
+                num1a = num2a;
+                num1b = num2b;
+                num2a = a;
+                num2b = b;
+        }
+        printf("\n");
+        return (0);
 }
