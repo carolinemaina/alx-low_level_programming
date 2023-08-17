0819b52e19acef499c5c4616d63f532498e70ad6
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-08-17 15:19:01 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int a, b;
+	int n1, n2;
 	char *op;
 
 	if (argc != 4)
@@ -20,9 +20,9 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	a = atoi(argv[1]);
+	n1 = atoi(argv[1]);
 	op = argv[2];
-	b = atoi(argv[3]);
+	n2 = atoi(argv[3]);
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
@@ -30,12 +30,12 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(99);
 	}
 
-	if ((*op == '/' && b == 0) || (*op == '%' && b == 0))
+	if ((*op == '/' && n2 == 0) || (*op == '%' && n2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(a, b));
+	printf("%d\n", get_op_func(op)(n1, n2));
 	return (0);
 }
