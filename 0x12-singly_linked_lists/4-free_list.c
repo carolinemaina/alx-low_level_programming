4de9cdc349ffabca9186066977470be38dd3130f
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-08-24 13:04:11 -0700

    function that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free-list - function that frees a list_t list
+ * @head: list to be freed
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *buff;
+
+	while (head)
+	{
+		buff = head->next;
+		free(head->str);
+		free(head);
+		head = buff;
+	}
+}
