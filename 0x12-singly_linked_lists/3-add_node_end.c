584948b2cb2fce947ff8d6cd17a33386784ecfbe
carolinemaina <57516721+carolinemaina@users.noreply.github.com>
2023-08-24 13:18:43 -0700

    address of the new element, or NULL if it failed

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end - function that adds a new node at the
+ * end of a list_t list
+ * @head: double pointer to list_t
+ * @str: string for new node
+ * Return: address of the new element, or NULL if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *n;
+	list_t *buff = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+	n = malloc(sizeof(list_t));
+	if (!n)
+		return (NULL);
+
+	n->str = strdup(str);
+	n->len = len;
+	n->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = n;
+		return (n);
+	}
+
+	while (buff->next)
+		buff = buff->next;
+	buff->next = n;
+
+	return (n);
+}
